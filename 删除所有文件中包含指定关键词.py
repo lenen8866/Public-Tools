085d42a0f7dce933e04a3e067246983cb570cf06
lenen <lenen8866@gmail.com>
2024-12-14 10:42:25 +0800

    输入要清空的目录路径,(可输入多个目录路径)

--- 删除所有文件中包含指定关键词.py
+++ 删除所有文件中包含指定关键词.py
@@ -1,3 +1,14 @@
+# file1.txt
+# file2@@@.txt
+# file3.html
+# example@@@.txt
+# other_file.txt
+
+# 运行代码后，以下文件会被删除：
+# 	•	file2@@@.txt
+# 	•	example@@@.txt
+
+
 import os
 
 def delete_txt_files_with_processed(directory):
@@ -7,12 +18,12 @@ def delete_txt_files_with_processed(directory):
             # 说明endswith(""),删除没有尾缀的文件
             #可以设置任何的尾缀的文件file.endswith("txt"),("html"),
             #而“_processed”是属于关键词,文件名的关键词,可以随意改要删除的.
-            if file.endswith("txt") and "\<break time=" in file:  # 检查.txt文件名中是否包含"_processed"
+            if file.endswith("txt") and "@@@" in file:  # 检查.txt文件名中是否包含"_processed"
                 file_path = os.path.join(root, file)
                 print(f"Deleting file: {file_path}")
                 os.remove(file_path)
 
 if __name__ == "__main__":
     # 在这里指定你想要扫描的目录
-    directory = "/Users/jianxinwei/Desktop/临时/转音频<Speak>文件"  # 替换为你的实际目录路径
+    directory = "story/纯文本文件"  # 替换为你的实际目录路径
     delete_txt_files_with_processed(directory)
